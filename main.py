cd46cb4a9f2c8c624a3de822626e0d623134f8d1
Neel Adwani <nprosofficial@gmail.com>
2021-06-27 22:11:15 +0000

    Created separate route

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 
+
+
 app = Flask(
   __name__,
   template_folder='templates',
@@ -10,6 +12,15 @@ app = Flask(
 @app.route('/')
 def index():
   return render_template('index.html')
+
+
+
+# Creating different routes
+@app.route('/second')
+def second():
+  return "I'm on a separate route"
+
+
 
 if __name__ == '__main__':
   # Run the Flask app
